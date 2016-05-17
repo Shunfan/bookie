ef4605facf9a69e022dd59768c0ed2a5d027076e
Shunfan Du <i@perry.asia>
2016-05-17 18:34:41 -0400

    Remove exposed API key

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   port: 8080,
   secret: 'it-is-a-secret',
   saltRounds: 10,
-  sendGridAPIKey: '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'
+  sendGridAPIKey: ''
 };
